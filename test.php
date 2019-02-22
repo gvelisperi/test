e0b67751b2f58bca67767b1ca09c028c66f262ba
Nika Dateshidze <mikoblaus@gmail.com>
2019-02-22 17:00:22 +0300

    second comm

--- test.php
+++ test.php
@@ -6,7 +6,9 @@
     <h1> gitStep 1</h1>
     <p>Some Text
     </p>
-
+    <h1> gitStep 2</h1>
+    <p>Some TextSome TextSome TextSome TextSome TextSome TextSome TextSome Text
+    </p>
 
 </body>
 
